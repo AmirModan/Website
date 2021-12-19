d0ba7b8a6f2957771b75214dfd1bf392eda0c81b
AmirModan <52586334+AmirModan@users.noreply.github.com>
2021-12-19 00:37:03 -0800

    Add files via upload

---
+++ Projects/src/TM4C/Quake_Detector/Objects/pwm.d
@@ -0,0 +1,13 @@
+.\objects\pwm.o: PWM.c
+.\objects\pwm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\pwm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\pwm.o: PWM.h
+.\objects\pwm.o: inc/hw_types.h
+.\objects\pwm.o: inc/hw_memmap.h
+.\objects\pwm.o: inc/hw_gpio.h
+.\objects\pwm.o: driverlib/sysctl.h
+.\objects\pwm.o: driverlib/pin_map.h
+.\objects\pwm.o: driverlib/rom_map.h
+.\objects\pwm.o: driverlib/gpio.h
+.\objects\pwm.o: driverlib/pwm.h
+.\objects\pwm.o: inc/tm4c123gh6pm.h
