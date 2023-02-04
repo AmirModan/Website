e605e56381971e83de34fc83bc9ca4cbc48778ee
Amir Modan <amir5modan@gmail.com>
2023-02-03 18:50:41 -0800

    Increased Mobile Threshold to 700px

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3ed8a24e1d49be3"; }
+/******/ 		__webpack_require__.h = function() { return "28f98a958e41f089"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
